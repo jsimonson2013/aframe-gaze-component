fce6e912b55fb4445fa6a3799d641416aa477a03
jsimonson <jsimonson2013@gmail.com>
2017-05-23 13:21:11 -0500

    Added code from proof-of-concept project.

--- index.js
+++ index.js
@@ -8,17 +8,26 @@ if (typeof AFRAME === 'undefined') {
  * Gaze Control component for A-Frame.
  */
 AFRAME.registerComponent('gaze-control', {
-  schema: {},
+
+  schema: {
+    enabled: {default: true},
+    widthSectors: {type: 'number', default: 3},
+    heightSectors: {type: 'number', default: 3}
+  },
 
   /**
    * Set if component needs multiple instancing.
    */
   multiple: false,
 
+
   /**
    * Called once when component is attached. Generally for initial setup.
    */
-  init: function () { },
+  init: function () {
+    this.previousPosition = new THREE.Vector3();
+    this.deltaPosition = new THREE.Vector3();
+  },
 
   /**
    * Called when component is attached and when component data changes.
@@ -30,7 +39,9 @@ AFRAME.registerComponent('gaze-control', {
    * Called when a component is removed (e.g., via removeAttribute).
    * Generally undoes all modifications to the entity.
    */
-  remove: function () { },
+  remove: function () {
+    this.pause();
+  },
 
   /**
    * Called on each scene tick.
@@ -41,11 +52,48 @@ AFRAME.registerComponent('gaze-control', {
    * Called when entity pauses.
    * Use to stop or remove any dynamic or background behavior such as events.
    */
-  pause: function () { },
+  pause: function () {
+    var sceneEl = this.el.sceneEl;
+    var canvasEl = sceneEl.canvas;
+
+    canvasEl.removeEventListener('gazemove', this.onGazeMove);
+  },
 
   /**
    * Called when entity resumes.
    * Use to continue or add any dynamic or background behavior such as events.
    */
-  play: function () { }
+  play: function () {
+    var sceneEl = this.el.sceneEl;
+    var canvasEl = sceneEl.canvas;
+
+    this.previousPosition.set(0, 0, 0);
+    canvasEl.addEventListener('gazemove', this.onGazeMove);
+  },
+
+
+  onGazeMove: function(event) {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    var sector_height = height/4; // height dimension of sector
+    var sector_width = width/6; // width dimension of sector
+
+    var movement_speed = 25; // speed of panning
+
+    // Adapted from onMouseMove()
+    var pitchObject = this.pitchObject;
+    var yawObject = this.yawObject;
+    var previousGazeEvent = this.previousGazeEvent;
+    if (!this.data.enabled) { return; }
+
+    var movementX = 1;
+    var movementY = 1;
+
+    // Adapted from onMouseMove()
+    this.previousGazeEvent = event;
+    yawObject.rotation.y -= movementX * 0.002;
+    pitchObject.rotation.x -= movementY * 0.002;
+    pitchObject.rotation.x = Math.max(-PI_2, Math.min(PI_2, pitchObject.rotation.x));
+  }
 });
