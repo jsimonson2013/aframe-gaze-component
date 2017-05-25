173b8b27febf951061df2e602cbe1d1b234cce87
jsimonson <jsimonson2013@gmail.com>
2017-05-25 14:03:34 -0500

    Removed a lot of code pertaining to mouse controls.

--- index.js
+++ index.js
@@ -20,9 +20,15 @@ AFRAME.registerComponent('gaze-control', {
    init: function () {
      this.previousPosition = new THREE.Vector3();
      this.deltaPosition = new THREE.Vector3();
-     this.setupMouseControls();
+
+     // From setupMouseControls
+     this.pitchObject = new THREE.Object3D();
+     this.yawObject = new THREE.Object3D();
+     this.yawObject.position.y = 10;
+     this.yawObject.add(this.pitchObject);
+
      this.setupHMDControls();
-     this.bindMethods();
+     this.onGazeMove = this.onGazeMove.bind(this);
    },
 
    update: function () {
@@ -50,22 +56,6 @@ AFRAME.registerComponent('gaze-control', {
    },
 
    bindMethods: function () {
-     this.onMouseDown = this.onMouseDown.bind(this);
-     this.onGazeMove = this.onGazeMove.bind(this);
-     this.onMouseMove = this.onMouseMove.bind(this);
-     this.releaseMouse = this.releaseMouse.bind(this);
-     this.onTouchStart = this.onTouchStart.bind(this);
-     this.onTouchMove = this.onTouchMove.bind(this);
-     this.onTouchEnd = this.onTouchEnd.bind(this);
-   },
-
-   setupMouseControls: function () {
-     // The canvas where the scene is painted
-     this.mouseDown = false;
-     this.pitchObject = new THREE.Object3D();
-     this.yawObject = new THREE.Object3D();
-     this.yawObject.position.y = 10;
-     this.yawObject.add(this.pitchObject);
    },
 
    setupHMDControls: function () {
@@ -85,18 +75,6 @@ AFRAME.registerComponent('gaze-control', {
        return;
      }
 
-     // Mouse Events
-     canvasEl.addEventListener('mousedown', this.onMouseDown, false);
-     canvasEl.addEventListener('mousemove', this.onMouseMove, false);
-     canvasEl.addEventListener('mouseup', this.releaseMouse, false);
-     canvasEl.addEventListener('mouseout', this.releaseMouse, false);
-
-     // Touch events
-     canvasEl.addEventListener('touchstart', this.onTouchStart);
-     canvasEl.addEventListener('touchmove', this.onTouchMove);
-     canvasEl.addEventListener('touchend', this.onTouchEnd);
-
-     // Gaze events
      canvasEl.addEventListener('gazemove', this.onGazeMove);
    },
 
@@ -105,18 +83,6 @@ AFRAME.registerComponent('gaze-control', {
      var canvasEl = sceneEl && sceneEl.canvas;
      if (!canvasEl) { return; }
 
-     // Mouse Events
-     canvasEl.removeEventListener('mousedown', this.onMouseDown);
-     canvasEl.removeEventListener('mousemove', this.onMouseMove);
-     canvasEl.removeEventListener('mouseup', this.releaseMouse);
-     canvasEl.removeEventListener('mouseout', this.releaseMouse);
-
-     // Touch events
-     canvasEl.removeEventListener('touchstart', this.onTouchStart);
-     canvasEl.removeEventListener('touchmove', this.onTouchMove);
-     canvasEl.removeEventListener('touchend', this.onTouchEnd);
-
-     // Gaze events
      canvasEl.removeEventListener('gazemove', this.onGazeMove);
    },
 
@@ -260,62 +226,5 @@ AFRAME.registerComponent('gaze-control', {
      yawObject.rotation.y -= movementX * 0.002;
      pitchObject.rotation.x -= movementY * 0.002;
      pitchObject.rotation.x = Math.max(-PI_2, Math.min(PI_2, pitchObject.rotation.x));
-   },
-
-   onMouseMove: function (event) {
-     var pitchObject = this.pitchObject;
-     var yawObject = this.yawObject;
-     var previousMouseEvent = this.previousMouseEvent;
-
-     if (!this.mouseDown || !this.data.enabled) { return; }
-
-     var movementX = event.movementX || event.mozMovementX;
-     var movementY = event.movementY || event.mozMovementY;
-
-     if (movementX === undefined || movementY === undefined) {
-       movementX = event.screenX - previousMouseEvent.screenX;
-       movementY = event.screenY - previousMouseEvent.screenY;
-     }
-     this.previousMouseEvent = event;
-
-     yawObject.rotation.y -= movementX * 0.002;
-     pitchObject.rotation.x -= movementY * 0.002;
-     pitchObject.rotation.x = Math.max(-PI_2, Math.min(PI_2, pitchObject.rotation.x));
-   },
-
-   onMouseDown: function (event) {
-     this.mouseDown = true;
-     this.previousMouseEvent = event;
-   },
-
-   releaseMouse: function () {
-     this.mouseDown = false;
-   },
-
-   onTouchStart: function (e) {
-     if (e.touches.length !== 1) { return; }
-     this.touchStart = {
-       x: e.touches[0].pageX,
-       y: e.touches[0].pageY
-     };
-     this.touchStarted = true;
-   },
-
-   onTouchMove: function (e) {
-     var deltaY;
-     var yawObject = this.yawObject;
-     if (!this.touchStarted) { return; }
-     deltaY = 2 * Math.PI * (e.touches[0].pageX - this.touchStart.x) /
-             this.el.sceneEl.canvas.clientWidth;
-     // Limits touch orientaion to to yaw (y axis)
-     yawObject.rotation.y -= deltaY * 0.5;
-     this.touchStart = {
-       x: e.touches[0].pageX,
-       y: e.touches[0].pageY
-     };
-   },
-
-   onTouchEnd: function () {
-     this.touchStarted = false;
    }
  });
